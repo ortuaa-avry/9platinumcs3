9176e761138e9964d17c00cc5a426e5e35ea1378
avryelle <abortua@brc.pshs.edu.ph>
2026-09-10 12:40:46 +0800

    Implement Movie class with methods and examples
    Added a new Movie class with methods for playback, presenting, and releasing movies. Created two movie objects and demonstrated their availability status before and after calling the release method.

--- q1/classRelationships.py
+++ q1/classRelationships.py
@@ -1,1 +1,47 @@
+class Movie:
+  def __init__(self, title, genre, availability, director, year, duration):
+  #Public Attributes
+    self.title = title
+    self.genre = genre
+    self.availability = availability
+  #Private Attributes
+    self.__director = director
+    self.__year = year
+    self.__duration = duration
+#Method 1
+  def playback(self):
+    print(f"Now Playing: {self.title} ({self.__year})")
+    print(f"Directed by: {self.__director}") 
+    print(f"Duration: {self.__duration}hrs")
+#Method 2
+  def present(self, new_title):
+    self.title = new_title
+    self.availability = True
+    print(f"Movie is updated to: {self.title}")
+#Method 3
+  def release(self):
+    self.availability = True
+    print(f"'{self.title}' is now available to watch!")
 
+#Object 1 I LOVE MAMMA MIAAAA
+movie1 = Movie("Mamma Mia!", "Musical/Comedy", False, "Phyllida Llyod", 2008, 1.48)
+#Object 2 MERYL STREEP DA GOAT
+movie2 = Movie("The Devil Wears Prada", "Comedy/Drama", True, "David Frankel", 2006, 1.49)
+
+#Status before object 1 was changed
+print("BEFORE:")
+print(f"Movie 1: {movie1.title}, Available: {movie1.availability}")
+print(f"Movie 2: {movie2.title}, Available: {movie2.availability}\n")
+
+#Method on Object 1 only
+movie1.release()
+print()
+
+#Final Status
+print("AFTER:")
+print(f"Movie 1: {movie1.title}, Available: {movie1.availability}")
+print(f"Movie 2: {movie2.title}, Available: {movie2.availability}")
+
+class Movie:
+  def __init__(self, title, genre, availability, composer, year, duration):
+    self.Song = None
